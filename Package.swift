22b088e21930a735227c2d97419a04cb2ea750cb
Ben Chatelain <ben@octop.ad>
2021-12-30 20:44:46 -0700

    🐛 Fix package syntax

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
 
             // url: "https://software.ditto.live/cocoa/DittoSwift/1.0.19/dist/DittoSwift-dist.tar.gz",
             url: "https://github.com/phatblat/Ditto.swift/releases/download/1.0.19/DittoSwift.xcframework.zip",
-            checksum: "93c65ae87fdd43eb1282bffc33047f543ba8e00d49fbf97bd4c5118be4d5d76b")
+            checksum: "93c65ae87fdd43eb1282bffc33047f543ba8e00d49fbf97bd4c5118be4d5d76b"
         )
     ]
 )
